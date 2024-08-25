df9fc84f7a775e48bd13b111373fc2661771b786
oriel blackmark <kiplangatoriel@gmail.com>
2024-08-26 00:28:03 +0300

    Modified the login container

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,12 +3,18 @@ import "./Style.css";
 function Login() {
   return (
     <div className="form">
+      <div className="title"><h2>Enter your Details to Login</h2></div>
       <label htmlFor="">Username</label>
-      <input type="text" />
+      <input type="text" required />
       <label htmlFor="">Password</label>
-      <input type="password" />
+      <input type="password" required />
       <label htmlFor=""></label>
-      <input type="submit" value="Login" defaultValue="Login" className="button" />
+      <input
+        type="submit"
+        value="Login"
+        defaultValue="Login"
+        className="button"
+      />
     </div>
   );
 }
